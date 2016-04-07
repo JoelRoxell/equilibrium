d80bd7745cbd01d56fbdbf6ad0146234b3c119b3
joelroxell <joel.roxell@na-kd.com>
2016-04-07 23:01:42 +0200

    [Add, Update] Included required stylus loader, poststylus, included stylus loader in webpack config and updated old style sheet files.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Title from 'components/title'
-import css from './styles/main.css' // eslint-disable-line
+import style from 'styles/main' // eslint-disable-line
 
-ReactDOM.render(<Title title='test' />, document.getElementById('app'))
+ReactDOM.render(<Title title='Thank God it is friday!' />, document.getElementById('app'))
 
 if (module.hot) {
   module.hot.accept()
